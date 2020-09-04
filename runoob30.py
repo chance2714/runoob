896b91f69dc84fb6aca11846e708a85f3269b937
chance2714 <zhanghaozi1343@163.com>
2020-09-04 16:21:20 +0800

    add runoob

---
+++ runoob30.py
@@ -0,0 +1,8 @@
+a = input('please input 5')
+if a[0] == a[-1]:
+    if a[1] == a[-2]:
+        print('yes')
+    else:
+        print('no')
+else:
+    print('no')
---
+++ runoob33*.py
@@ -0,0 +1,4 @@
+i = input('input a string')
+ln = []
+for j in i:
+    ln.append(i)
