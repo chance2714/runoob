6db1ec5a6d60dd753140edc7d311b3e2835cdd0f
chance2714 <zhanghaozi1343@163.com>
2020-09-07 11:51:14 +0800

    9.7 updated

---
+++ runoob37.py
@@ -0,0 +1,6 @@
+l = []
+for num in range(1, 10):
+    i = input('enter num {}:'.format(num))
+    print(i)
+    l.append(i)
+print(sorted(l))
---
+++ runoob39.py
@@ -0,0 +1,5 @@
+l = [1,3,5,10,44,65,100,300]
+num = input('insert a num: ')
+l.append(int(num))
+print(l)
+print('after sorted: ',sorted(l))
