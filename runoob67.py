2fa4d7757e818e4bb639595ed4f0767a2edd49fa
chance2714 <zhanghaozi1343@163.com>
2020-09-11 17:12:00 +0800

    9.11

--- runoob67.py
+++ runoob67.py
@@ -1,15 +1,23 @@
 def inp():
-    lo = input('please input a serveral numbers')
-    li = list(lo)
-    l = [int(x) for x in li]
-def maxium(a,b):
-    a,b = b,a
-    return (a,b)
-def minium(c,d):
-    c,d = d,c
-    return (c,d)
+    Numbers = input('please input a serveral numbers')
+    List_str = list(Numbers)
+    List_int = [int(x) for x in List_str]
+
+def maxium(MaxNum,FirstNum):
+    indexMax = List_int.index(MaxNum)
+    List_int[indexMax],List_int[0] = FirstNum,MaxNum
+    return (List_int)
+
+def minium(MinNum,LastNum):
+    indexMin = List_int.index(MinNum)
+    List_int[indexMin],List_int[-1] = LastNum,MinNum
+    return (List_int)
+
 if __name__ == '__main__':
-    inp()
-    maxium(max(l),l[0])
-    minium(min(l),l[-1])
+    Numbers = input('please input a serveral numbers: ')
+    List_int = [int(x) for x in str(Numbers)]
+    maxium(max(List_int),List_int[0])
+    print(List_int)
+    minium(min(List_int),List_int[-1])
+    print(List_int)
 
